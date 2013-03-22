b030f759bcec805e02403ee4e86d434bb9e1adad
bryan kennedy <bkennedy@smm.org>
2013-03-22 15:30:17 -0500

    Adding a basic config file
    Copying only the smallest portion of the NPR app-template since I want
    to see what we need and only expand from there.

---
+++ app_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Project-wide application configuration.
+
+DO NOT STORE SECRETS, PASSWORDS, ETC. IN THIS FILE.
+They will be exposed to users. Use environment variables instead.
+"""
+
+PROJECT_NAME = 'App Template'
+PROJECT_SLUG = 'app-template'
+REPOSITORY_NAME = 'app-template'
+
