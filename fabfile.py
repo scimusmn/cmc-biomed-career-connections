1d1f395368d9b57dbf84cd26fc814d8341649ee5
bryan kennedy <bkennedy@smm.org>
2013-03-22 14:58:08 -0500

    Basic fabric setup
    Starting an app template for some testing with exhibit kiosks

---
+++ fabfile.py
@@ -0,0 +1,9 @@
+"""
+Template for SMM applications
+
+This can be used for web projects, browser kiosks, custom interactives, etc.
+
+"""
+
+from fabric.api import *
+from fabric.contrib.console import confirm
