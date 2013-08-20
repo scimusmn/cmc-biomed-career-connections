bd70a2e0b556c4ade65170e52700b762cd0a0a89
bryan kennedy <bkennedy@smm.org>
2013-08-20 16:21:33 -0500

    Initial strucutre
    This doesn't do much right now, just setting up the basic strucuture
    that exists so far.

--- fabfile.py
+++ fabfile.py
@@ -7,3 +7,28 @@ This can be used for web projects, browser kiosks, custom interactives, etc.
 
 from fabric.api import *
 from fabric.contrib.console import confirm
+
+import app_config
+
+env.repo_name = app_config.REPOSITORY_NAME
+
+
+def _header(txt):
+    wrapper = "------------------------------------------------------"
+    return wrapper + "\n" + txt + "\n" + wrapper
+
+
+@task
+def hello_run():
+    """
+    Remote hello world for troubleshooting
+    """
+    run("echo \"Hello World\"")
+
+
+@task
+def app_details():
+    """
+    Print the project details
+    """
+    print _header("Repo name: " + env.repo_name)
