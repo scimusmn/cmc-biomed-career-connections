1dce5109300ff7ded1479cde74357b809202ebae
bryan kennedy <bkennedy@smm.org>
2014-05-09 16:26:56 -0500

    Import a very basic Bacbone view.
    Require Backbone, underscore, and jquery with require.js
    
    Then send a simple console message when we load a very basic view.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,34 @@
 require.config({
-    baseUrl: 'vendor/bower/jquery/dist',
+    //baseUrl: 'vendor/bower/jquery/dist',
+
+    enforceDefine: true,
     paths: {
-        jquery: 'jquery.min'
+        'jquery': 'vendor/jquery/dist/jquery',
+        'modernizer': 'vendor/modernizer/modernizer',
+        'underscore': 'vendor/underscore/underscore',
+        'backbone': 'vendor/backbone/backbone'
+    },
+    shim: {
+        "underscore": {
+            deps: [],
+            exports: "_"
+        },
+        "backbone": {
+            deps: ["jquery", "underscore"],
+            exports: "Backbone"
+        },
     }
 });
+
+require(['views/app'], function(AppView) {
+    new AppView;
+});
+
+define(["jquery", "underscore", "backbone"],
+    function ($, _, Backbone) {
+        console.log("Test output");
+        console.log("$: " + typeof $);
+        console.log("_: " + typeof _);
+        console.log("Backbone: " + typeof Backbone);
+    }
+);
