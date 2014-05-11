efc154f6cc13522bbfccb727565c698b6f5358cc
bryan kennedy <bkennedy@smm.org>
2014-05-11 16:46:03 -0500

    Adding MVC for the app
    This is based on the structure provided by a user directory back bone
    application. That is why there is some cruft from there.

---
+++ app/scripts/app/router.js
@@ -0,0 +1,38 @@
+define(function (require) {
+
+    "use strict";
+
+    var $           = require('jquery'),
+        Backbone    = require('backbone'),
+        ShellView   = require('app/views/Shell'),
+        HomeView    = require('app/views/Home'),
+
+        $body = $('body'),
+        shellView = new ShellView({el: $body}).render(),
+        $content = $("#content", shellView.el),
+        homeView = new HomeView({el: $content});
+
+    return Backbone.Router.extend({
+
+        routes: {
+            "": "home",
+            "contact": "contact",
+        },
+
+        home: function () {
+            homeView.delegateEvents(); // delegate events when the view is recycled
+            homeView.render();
+            shellView.selectMenuItem('home-menu');
+        },
+
+        contact: function () {
+            require(["app/views/Contact"], function (ContactView) {
+                var view = new ContactView({el: $content});
+                view.render();
+                shellView.selectMenuItem('contact-menu');
+            });
+        },
+
+    });
+
+});
