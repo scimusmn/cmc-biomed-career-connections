994e4fe4fd752b1845b4e2de9810a6d75c729b4e
Brandon <brandonwkipp@gmail.com>
2020-12-03 13:07:21 -0600

    refactor: Fixed arduino-base install conditional, simplified arduino-base & ReactSerial installation

--- scripts/install-arduino-base.js
+++ scripts/install-arduino-base.js
@@ -2,10 +2,13 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 
 // Setup dir structure and install submodule for arduino-base if it does not exist
-if (fs.existsSync('src/Arduino/arduino-base/ReactSerial')) {
-  execSync('cd src && mkdir Arduino');
+if (!fs.existsSync('src/Arduino/arduino-base/ReactSerial')) {
+  execSync('cd src && mkdir -p Arduino');
   execSync('cd src/Arduino && git submodule add https://github.com/scimusmn/arduino-base');
 }
 
 // Get the latest submodule reference
 execSync('git submodule update --init');
+
+// Install/update arduino-base ReactSerial library dependencies
+execSync('cd src/Arduino/arduino-base/ReactSerial && yarn');
