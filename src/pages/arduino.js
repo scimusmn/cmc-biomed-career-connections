fea96f0a6a35b5fb22534bb8e92e34078a682a68
tnordberg <tnordberg@smm.org>
2020-12-09 18:08:00 -0600

    Revert "Gatsby restructure and adding Communication Debug Page."
    This reverts commit e07f447aac974dc24ac5c8fcb15869665170e370.

--- src/pages/arduino.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import ArduinoTestPage from '../Arduino/arduino-base/ReactSerial/examples/arduino-test-page';
-
-const ArduinoWrapperPage = () => (
-  <ArduinoTestPage />
-);
-
-export default ArduinoWrapperPage;
