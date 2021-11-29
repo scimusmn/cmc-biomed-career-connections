604cb80358ade29d60b23ff81f36fd66e946e6f9
trig-vee <tnordberg@smm.org>
2021-11-29 16:38:57 -0600

    Stripping out bootstrap and reactstrap
    Making app-template less opinionated about which style/UX frameworks should be used.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '@styles/base.scss';
 
 export const wrapRootElement = ({ element }) => <>{element}</>;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
 
 const Home = () => (
-  <Container>
-    <Row>
-      <Col>
-        <h1>Home</h1>
-        <p>Start your app here.</p>
-      </Col>
-    </Row>
-  </Container>
+  <div>
+    <h1>Home</h1>
+    <p>Start your app here.</p>
+  </div>
 );
 
 export default Home;
