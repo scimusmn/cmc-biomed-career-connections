4167967b2bbf2652e67aa84c2d9070beb5029190
trig-vee <tnordberg@smm.org>
2021-11-29 18:31:04 -0600

    chore: remove all sass usages

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,8 +2,7 @@
 // Don't require a default export. Gatsby's API can't support it here.
 import PropTypes from 'prop-types';
 import React from 'react';
-
-import '@styles/base.scss';
+import './src/styles/index.css';
 
 export const wrapRootElement = ({ element }) => <>{element}</>;
 
